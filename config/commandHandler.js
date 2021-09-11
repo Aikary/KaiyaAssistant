27e906553cbe2876fb758b389fb2dd510d5a260d
zAzkkya <90440005+zAzkkya@users.noreply.github.com>
2021-09-11 16:05:17 +0700

    Add files via upload

---
+++ config/commandHandler.js
@@ -0,0 +1,14 @@
+const { Collection } = require("discord.js")
+const fs = require("fs");
+
+const commands = new Collection();
+const files = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of files) {
+    const commands = require(`../commands/${file}`);
+    commands.set(commands.name, command);
+}
+
+module.exports = commands;
