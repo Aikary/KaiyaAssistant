e10cbd0457ab4fc84bc1831d4c47c68fc0d2f810
zAzkkya <90440005+zAzkkya@users.noreply.github.com>
2021-09-11 16:04:26 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,7 @@
-fs
+module.exports = {
+    name: 'ready',
+    execute(client) {
+        console.log("AkyraDiscordBot Is Online Yay!");
+        console.log(`logged in as ${client.user.tag}!`);
+    }
+}
