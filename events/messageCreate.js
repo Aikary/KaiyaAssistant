832d6e17b50d189197f6a22f14eccb38160abc6c
zAzkkya <90440005+zAzkkya@users.noreply.github.com>
2021-09-11 16:03:59 +0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,20 @@
-f
+const commands = require("../config/commandHandler");
+const config = require("../utils/config.json");
+
+module.exports = {
+    name: 'MessageCreate',
+    execute(msg, client) {
+        if (msg.author.bot) return;
+
+        let args = "";
+    
+        config.prefix.forEach((prefix) => {
+            if (msg.content.startsWith(prefix)) {
+               args = msg.content.substring(prefix.length).split(" "); 
+            }
+        });
+    
+        if (!args) return;
+        commands.get(args[0]).execute(msg, args);
+    }
+}
