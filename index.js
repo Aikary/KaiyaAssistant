732a63f799f1cd6e744279d0637917ed97c01c0b
zAzkkya <90440005+zAzkkya@users.noreply.github.com>
2021-09-11 16:02:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-const { Client, Collection, Intents } = require("discord.js");
+const { Client, Intents } = require("discord.js");
 const fs = require("fs");
 const config = require("./utils/config.json");
 
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILDS_MESSAGES],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILDS_MESSAGES],
 });
 
-client.on("ready", () => {
-    console.log("AkyraDiscordBot Is Online Yay!");
-    console.log(`logged in as ${client.user.tag}!`);
-});
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file)=>file.endsWith(".js"));
 
-client.on("message.Create", (msg) => {
-    if (msg.author.bot) return;
-
-    let args = "";
-
-    config.prefix.forEach((prefix) => {
-        if (msg.content.startsWith(prefix)) {
-           args = msg.content.substring(prefix.length).split(" "); 
-        }
-    });
-
-    if (!args) return;
-    client.commands.get(args[0]).execute(msg, args);
-});
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (args) => event.execute(args, client));
+  } else {
+    client.on(event.name, (args) => event.execute(args, client));
+  }   
+} 
 
 client.login(config.API_TOKEN);
